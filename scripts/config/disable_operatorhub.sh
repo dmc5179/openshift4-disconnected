4ad4cdfdaefbf88d0db46e1ce056b03fba8d29e0
Dan Clark <danclark@redhat.com>
2020-07-05 05:30:52 -0400

    Convert post install steps into individual scripts. Remove Ansible role that was originally intended for them

---
+++ scripts/config/disable_operatorhub.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Disable the OpenShift integrated OperatorHub.
+# Typically done for disconnected/air-gap installs until the disconnected
+# version of OperatorHub is installed
+
+oc patch OperatorHub cluster --type json -p '[{"op": "add", "path": "/spec/disableAllDefaultSources", "value": true}]'
