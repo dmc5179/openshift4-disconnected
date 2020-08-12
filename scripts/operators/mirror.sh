b687e17b8c8d838388d908afe75c3e02543e372a
Dan Clark <danclark@redhat.com>
2020-08-12 19:51:04 +0000

    Use env var and not hard coded user auth file location

--- scripts/operators/mirror.sh
+++ scripts/operators/mirror.sh
@@ -14,11 +14,11 @@ mkdir "${MANIFEST_PATH}"
 # Have to have a registry running somewhere just so this command can authenticate to it
 # and then do nothing
 oc adm catalog mirror --manifests-only \
-  --registry-config '/run/user/1000/containers/auth.json' \
+  --registry-config "${LOCAL_SECRET_JSON}" \
   --insecure=true --to-manifests=${MANIFEST_PATH} "${RH_OP_REPO}" "${LOCAL_REG}"
 
 cp "${MANIFEST_PATH}/mapping.txt" "${MANIFEST_PATH}/mapping.txt.orig"
 
 sed -i "s|localhost:5000|s3://s3.amazonaws.com/${AWS_DEFAULT_REGION}/${S3_BUCKET}|g" "${MANIFEST_PATH}/mapping.txt"
 
-cat "${MANIFEST_PATH}/mapping.txt" | xargs -n 1 -P ${THREADS} oc image mirror --registry-config '/run/user/1000/containers/auth.json' --insecure=true '{}'
+cat "${MANIFEST_PATH}/mapping.txt" | xargs -n 1 -P ${THREADS} oc image mirror --registry-config "${LOCAL_SECRET_JSON}" --insecure=true '{}'
