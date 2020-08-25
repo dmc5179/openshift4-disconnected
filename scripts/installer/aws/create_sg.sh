d68cc65d2abe15190a019f6d30a9d1f4d0feb386
Dan Clark <danclark@redhat.com>
2020-08-25 12:26:01 +0000

    Update script for creating complex security groups

--- scripts/installer/aws/create_sg.sh
+++ scripts/installer/aws/create_sg.sh
@@ -40,6 +40,21 @@ then
       --group-name "${OCP_CLUSTER_NAME}-k8s-master" \
       --vpc-id "${VPC_ID}"
 
+  rm -f /tmp/masters-sg.json
+  cp masters-sg.json /tmp/masters-sg.json
+  sed -i "s|MASTER_SG|${K8S_MASTER_SG}|g" /tmp/masters-sg.json
+  sed -i "s|WORKER_SG|${K8S_WORKER_SG}|g" /tmp/masters-sg.json
+  sed -i "s|AWS_ACCT_ID|${AWS_ACCT_ID}|g" /tmp/masters-sg.json
+  sed -i "s|CLUSTER_NAME|${OCP_CLUSTER_NAME}|g" /tmp/masters-sg.json
+  sed -i "s|VPC_ID|${VPC_ID}|g" /tmp/masters-sg.json
+  sed -i "s|K8S_ELB_SG|${K8S_ELB_SG}|g" /tmp/masters-sg.json
+
+  aws --endpoint-url "${EC2_ENDPOINT}" ${AWS_OPTS} ec2 authorize-security-group-ingress \
+      --group-id ${K8S_MASTER_SG} \
+      --cli-input-json "$(cat /tmp/masters-sg.json)"
+
+  #rm -f /tmp/masters-sg.json
+
 fi
 
 # If it doesn't exist, create it
@@ -51,29 +66,19 @@ then
       --group-name "${OCP_CLUSTER_NAME}-k8s-worker" \
       --vpc-id "${VPC_ID}"
 
+  rm -f /tmp/workers-sg.json
+  cp workers-sg.json /tmp/workers-sg.json
+  sed -i "s|MASTER_SG|${K8S_MASTER_SG}|g" /tmp/workers-sg.json
+  sed -i "s|WORKER_SG|${K8S_WORKER_SG}|g" /tmp/workers-sg.json
+  sed -i "s|AWS_ACCT_ID|${AWS_ACCT_ID}|g" /tmp/workers-sg.json
+  sed -i "s|CLUSTER_NAME|${OCP_CLUSTER_NAME}|g" /tmp/workers-sg.json
+  sed -i "s|VPC_ID|${VPC_ID}|g" /tmp/workers-sg.json
+  sed -i "s|K8S_ELB_SG|${K8S_ELB_SG}|g" /tmp/workers-sg.json
+
+  aws --endpoint-url "${EC2_ENDPOINT}" ${AWS_OPTS} ec2 authorize-security-group-ingress \
+      --group-id ${K8S_WORKER_SG} \
+      --cli-input-json "$(cat /tmp/workers-sg.json)"
+
 fi
-
-
-rm -f /tmp/masters-sg.json
-cp masters-sg.json /tmp/masters-sg.json
-sed -i "s|MASTER_SG|${K8S_MASTER_SG}|g" /tmp/masters-sg.json
-sed -i "s|WORKER_SG|${K8S_WORKER_SG}|g" /tmp/masters-sg.json
-sed -i "s|AWS_ACCT_ID|${AWS_ACCT_ID}|g" /tmp/masters-sg.json
-
-aws --endpoint-url "${EC2_ENDPOINT}" ${AWS_OPTS} ec2 authorize-security-group-ingress \
-    --group-id ${K8S_MASTER_SG} \
-    --cli-input-json "$(cat /tmp/masters-sg.json)"
-
-#rm -f /tmp/masters-sg.json
-
-rm -f /tmp/workers-sg.json
-cp workers-sg.json /tmp/workers-sg.json
-sed -i "s|MASTER_SG|${K8S_MASTER_SG}|g" /tmp/workers-sg.json
-sed -i "s|WORKER_SG|${K8S_WORKER_SG}|g" /tmp/workers-sg.json
-sed -i "s|AWS_ACCT_ID|${AWS_ACCT_ID}|g" /tmp/workers-sg.json
-
-aws --endpoint-url "${EC2_ENDPOINT}" ${AWS_OPTS} ec2 authorize-security-group-ingress \
-    --group-id ${K8S_WORKER_SG} \
-    --cli-input-json "$(cat /tmp/workers-sg.json)"
 
 exit 0
