b63417d832d9e8ed497e1952b4d67c076c002422
Dan Clark <danclark@redhat.com>
2020-08-26 00:28:23 +0000

    Updates to scripts for creating the OCP 4 infra in AWS

---
+++ scripts/installer/aws/route53.sh
@@ -0,0 +1,4 @@
+#!/bin/bash -xe
+
+#aws --endpoint-url 'https://route53.c2s.ic.gov' ${AWS_OPTS} route53 change-resource-record-sets --hosted-zone-id "${HOSTED_ZONE}" --change-batch file://route53.json
+
