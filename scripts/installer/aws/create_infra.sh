e74e4189c074197edf6371388add48f082279843
Dan Clark <danclark@redhat.com>
2020-08-26 00:28:23 +0000

    Updates to scripts for creating the OCP 4 infra in AWS

--- scripts/installer/aws/create_infra.sh
+++ scripts/installer/aws/create_infra.sh
@@ -61,120 +61,76 @@ aws --endpoint-url "${ELB_ENDPOINT}" ${AWS_OPTS} \
     --targets Id=${BOOTSTRAP_IP} Id=${MASTER0_IP} Id=${MASTER1_IP} Id=${MASTER2_IP}
 
 
-aws --endpoint-url "${ELB_ENDPOINT}" ${AWS_OPTS} \
-    elbv2 create-load-balancer --name "${OCP_CLUSTER_NAME}-int" \
-    --type network --subnets "${SUBNET_ID}" --security-groups "${SG_ID}"
 
-aws --endpoint-url "${ELB_ENDPOINT}" ${AWS_OPTS} \
-    elbv2 create-load-balancer --name "${OCP_CLUSTER_NAME}-ext}" \
-    --type network --subnets "${SUBNET_ID}" --security-groups "${SG_ID}"
+# Check if the security group exists already.
+K8S_ELB_SG=$(aws --endpoint-url "${EC2_ENDPOINT}" ${AWS_OPTS} ec2 describe-security-groups \
+  | jq '.SecurityGroups[] | select(.GroupName == "caas-k8s-elb") | .GroupId' | tr -d '"')
 
-aws --endpoint-url "${ELB_ENDPOINT}" ${AWS_OPTS} \
-    elbv2 create-load-balancer --name "${OCP_CLUSTER_NAME}-ingress" \
-    --type classic --subnets "${SUBNET_ID}" --security-groups "${SG_ID}"
+# Check if the security group exists already.
+K8S_MASTER_SG=$(aws --endpoint-url "${EC2_ENDPOINT}" ${AWS_OPTS} ec2 describe-security-groups \
+  | jq '.SecurityGroups[] | select(.GroupName == "caas-k8s-master") | .GroupId' | tr -d '"')
 
-
-
-
-#########
-
-MC_TG_ARN=$(aws --endpoint-url "${ELB_ENDPOINT}" ${AWS_OPTS} \
-    elbv2 describe-target-groups \
-    --name openshift-machineconfig \
-    --query 'TargetGroups[0].TargetGroupArn' --output text)
-
-aws --endpoint-url "${ELB_ENDPOINT}" ${AWS_OPTS} \
-    elbv2 register-targets \
-    --target-group-arn "${MC_TG_ARN}" \
-    --targets Id=10.0.106.50 Id=10.0.106.51 Id=10.0.106.52 Id=10.0.106.53
-
-aws --endpoint-url "${ELB_ENDPOINT}" ${AWS_OPTS} \
-    elbv2 create-target-group \
-    --name openshift-api \
-    --protocol TCP \
-    --port 6443 \
-    --target-type ip \
-    --vpc-id "${VPC_ID}"
-
-API_TG_ARN=$(aws --endpoint-url "${ELB_ENDPOINT}" ${AWS_OPTS} \
-    elbv2 describe-target-groups \
-    --name openshift-api \
-    --query 'TargetGroups[0].TargetGroupArn' --output text)
-
-aws --endpoint-url "${ELB_ENDPOINT}" ${AWS_OPTS} \
-    elbv2 register-targets \
-    --target-group-arn "${API_TG_ARN}" \
-    --targets Id=10.0.106.50 Id=10.0.106.51 Id=10.0.106.52 Id=10.0.106.53
-
-aws --endpoint-url "${ELB_ENDPOINT}" ${AWS_OPTS} \
-    elbv2 create-target-group \
-    --name openshift-ingress-http \
-    --protocol TCP \
-    --port 80 \
-    --target-type ip \
-    --vpc-id "${VPC_ID}"
-
-HTTP_TG_ARN=$(aws --endpoint-url "${ELB_ENDPOINT}" ${AWS_OPTS} \
-    elbv2 describe-target-groups \
-    --name openshift-ingress-http \
-    --query 'TargetGroups[0].TargetGroupArn' --output text)
-
-aws --endpoint-url "${ELB_ENDPOINT}" ${AWS_OPTS} \
-    elbv2 register-targets \
-    --target-group-arn "${HTTP_TG_ARN}" \
-    --targets Id=10.0.106.61 Id=10.0.106.62 Id=10.0.106.63
-
-aws --endpoint-url "${ELB_ENDPOINT}" ${AWS_OPTS} \
-    elbv2 create-target-group \
-    --name openshift-ingress-https \
-    --protocol TCP \
-    --port 443 \
-    --target-type ip \
-    --vpc-id "${VPC_ID}"
-
-HTTPS_TG_ARN=$(aws --endpoint-url "${ELB_ENDPOINT}" ${AWS_OPTS} \
-                   elbv2 describe-target-groups \
-                   --name openshift-ingress-https \
-                   --query 'TargetGroups[0].TargetGroupArn' --output text)
-
-aws --endpoint-url "${ELB_ENDPOINT}" ${AWS_OPTS} \
-    elbv2 register-targets \
-    --target-group-arn "${HTTPS_TG_ARN}" \
-    --targets Id=10.0.106.61 Id=10.0.106.62 Id=10.0.106.63
+# Check if the security group exists already.
+K8S_WORKER_SG=$(aws --endpoint-url "${EC2_ENDPOINT}" ${AWS_OPTS} ec2 describe-security-groups \
+  | jq '.SecurityGroups[] | select(.GroupName == "caas-k8s-worker") | .GroupId' | tr -d '"')
 
 
-aws --endpoint-url "${ELB_ENDPOINT}" ${AWS_OPTS} \
-    elbv2 create-load-balancer --name openshift-api-mc-lb \
-    --type network --subnets "${SUBNET_ID}" --security-groups "${SG_ID}"
+INT_LB_ARN=$(aws --endpoint-url "${ELB_ENDPOINT}" ${AWS_OPTS} \
+    elbv2 describe-load-balancers | jq '.LoadBalancers[] | select(.LoadBalancerName == "caas-int") | .LoadBalancerArn' | tr -d '"')
 
-API_LB_ARN=$(aws --endpoint-url "${ELB_ENDPOINT}" ${AWS_OPTS} \
-                 elbv2 describe-load-balancers --name openshift-api-mc-lb \
-                 --query 'LoadBalancers[0].LoadBalancerArn' --output text)
+if [ -z "${INT_LB_ARN}" ]
+then
 
-aws --endpoint-url "${ELB_ENDPOINT}" ${AWS_OPTS} \
-    elbv2 create-listener --load-balancer-arn "${API_LB_ARN}" --protocol TCP --port 6443 \
-    --default-actions Type=forward,TargetGroupArn=${API_TG_ARN}
+  aws --endpoint-url "${ELB_ENDPOINT}" ${AWS_OPTS} \
+      elbv2 create-load-balancer --name "${OCP_CLUSTER_NAME}-int" \
+      --type network --subnets "${SUBNET_ID}" --security-groups "${SG_ID}"
 
-aws --endpoint-url "${ELB_ENDPOINT}" ${AWS_OPTS} \
-    elbv2 create-listener --load-balancer-arn "${API_LB_ARN}" --protocol TCP --port 22623 \
-    --default-actions Type=forward,TargetGroupArn=${MC_TG_ARN}
+  INT_LB_ARN=$(aws --endpoint-url "${ELB_ENDPOINT}" ${AWS_OPTS} \
+      elbv2 describe-load-balancers | jq '.LoadBalancers[] | select(.LoadBalancerName == "caas-int") | .LoadBalancerArn' | tr -d '"')
 
-aws --endpoint-url "${ELB_ENDPOINT}" ${AWS_OPTS} \
-    elbv2 create-load-balancer --name openshift-ingress-lb --type network --subnets "${SUBNET_ID}" --security-groups "${SG_ID}"
+  # Add the target groups aint and sint
+  aws --endpoint-url "${ELB_ENDPOINT}" ${AWS_OPTS} \
+      elbv2 add-listener --load-balaner-arn "${INT_LB_ARN}" \
+      --port 6443 --protocol tcp \
+      --default-action "Type=forward,TargetGroupArn=${API_INT_TG_ARN}"
+
+  aws --endpoint-url "${ELB_ENDPOINT}" ${AWS_OPTS} \
+      elbv2 add-listener --load-balaner-arn "${INT_LB_ARN}" \
+      --port 22623 --protocol tcp \
+      --default-action "Type=forward,TargetGroupArn=${S_INT_TG_ARN}"
+fi
+
+
+EXT_LB_ARN=$(aws --endpoint-url "${ELB_ENDPOINT}" ${AWS_OPTS} \
+    elbv2 describe-load-balancers | jq '.LoadBalancers[] | select(.LoadBalancerName == "caas-ext") | .LoadBalancerArn' | tr -d '"')
+
+if [ -z "${EXT_LB_ARN}" ]
+then
+
+  aws --endpoint-url "${ELB_ENDPOINT}" ${AWS_OPTS} \
+      elbv2 create-load-balancer --name "${OCP_CLUSTER_NAME}-ext}" \
+      --type network --subnets "${SUBNET_ID}" --security-groups "${SG_ID}"
+
+  EXT_LB_ARN=$(aws --endpoint-url "${ELB_ENDPOINT}" ${AWS_OPTS} \
+      elbv2 describe-load-balancers | jq '.LoadBalancers[] | select(.LoadBalancerName == "caas-ext") | .LoadBalancerArn' | tr -d '"')
+
+  # Add the target group aext
+  aws --endpoint-url "${ELB_ENDPOINT}" ${AWS_OPTS} \
+      elbv2 add-listener --load-balaner-arn "${EXT_LB_ARN}" \
+      --port 6443 --protocol tcp \
+      --default-action "Type=forward,TargetGroupArn=${API_EXT_TG_ARN}"
+fi
 
 INGRESS_LB_ARN=$(aws --endpoint-url "${ELB_ENDPOINT}" ${AWS_OPTS} \
-                     elbv2 describe-load-balancers --name openshift-ingress-lb \
-                     --query 'LoadBalancers[0].LoadBalancerArn' --output text)
+    elb describe-load-balancers | jq '.LoadBalancers[] | select(.LoadBalancerName == "caas-ingress") | .LoadBalancerArn' | tr -d '"')
 
-aws --endpoint-url "${ELB_ENDPOINT}" ${AWS_OPTS} \
-    elbv2 create-listener --load-balancer-arn "${INGRESS_LB_ARN}" --protocol TCP --port 80 \
-    --default-actions Type=forward,TargetGroupArn=${HTTP_TG_ARN}
+if [ -z "${INGRESS_LB_ARN}" ]
+then
 
-aws --endpoint-url "${ELB_ENDPOINT}" ${AWS_OPTS} \
-    elbv2 create-listener --load-balancer-arn "${INGRESS_LB_ARN}" --protocol TCP --port 443 \
-    --default-actions Type=forward,TargetGroupArn=${HTTPS_TG_ARN}
+  aws --endpoint-url "${ELB_ENDPOINT}" ${AWS_OPTS} \
+      elb create-load-balancer --load-balancer-name "${OCP_CLUSTER_NAME}-ingress" \
+      --subnets "${SUBNET_ID}" --security-groups "${K8S_ELB_SG}"
 
+  # TODO: Add all nodes to the ingress load balancer above. Use IPs or instance IDs?
 
-aws --endpoint-url 'https://route53.c2s.ic.gov' ${AWS_OPTS} route53 change-resource-record-sets --hosted-zone-id "${HOSTED_ZONE}" --change-batch file://route53.json
-
-
+fi
