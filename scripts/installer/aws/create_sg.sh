6b6a1c6c2332ddbb3aaa70e4b62e9b0bd40a3605
Dan Clark <danclark@redhat.com>
2020-08-26 01:31:07 +0000

    Fix chick/egg problem in security groups creation

--- scripts/installer/aws/create_sg.sh
+++ scripts/installer/aws/create_sg.sh
@@ -25,6 +25,9 @@ then
       --group-name "${OCP_CLUSTER_NAME}-k8s-elb" \
       --vpc-id "${VPC_ID}"
 
+  K8S_ELB_SG=$(aws --endpoint-url "${EC2_ENDPOINT}" ${AWS_OPTS} ec2 describe-security-groups \
+    | jq '.SecurityGroups[] | select(.GroupName == "caas-k8s-elb") | .GroupId' | tr -d '"')
+
   aws --endpoint-url "${EC2_ENDPOINT}" ${AWS_OPTS} ec2 authorize-security-group-ingress \
       --group-id ${K8S_ELB_SG} \
       --cli-input-json "$(cat k8s-elb-sg.json)"
@@ -40,20 +43,8 @@ then
       --group-name "${OCP_CLUSTER_NAME}-k8s-master" \
       --vpc-id "${VPC_ID}"
 
-  rm -f /tmp/masters-sg.json
-  cp masters-sg.json /tmp/masters-sg.json
-  sed -i "s|MASTER_SG|${K8S_MASTER_SG}|g" /tmp/masters-sg.json
-  sed -i "s|WORKER_SG|${K8S_WORKER_SG}|g" /tmp/masters-sg.json
-  sed -i "s|AWS_ACCT_ID|${AWS_ACCT_ID}|g" /tmp/masters-sg.json
-  sed -i "s|CLUSTER_NAME|${OCP_CLUSTER_NAME}|g" /tmp/masters-sg.json
-  sed -i "s|VPC_ID|${VPC_ID}|g" /tmp/masters-sg.json
-  sed -i "s|K8S_ELB_SG|${K8S_ELB_SG}|g" /tmp/masters-sg.json
-
-  aws --endpoint-url "${EC2_ENDPOINT}" ${AWS_OPTS} ec2 authorize-security-group-ingress \
-      --group-id ${K8S_MASTER_SG} \
-      --cli-input-json "$(cat /tmp/masters-sg.json)"
-
-  #rm -f /tmp/masters-sg.json
+  K8S_MASTER_SG=$(aws --endpoint-url "${EC2_ENDPOINT}" ${AWS_OPTS} ec2 describe-security-groups \
+    | jq '.SecurityGroups[] | select(.GroupName == "caas-k8s-master") | .GroupId' | tr -d '"')
 
 fi
 
@@ -66,19 +57,37 @@ then
       --group-name "${OCP_CLUSTER_NAME}-k8s-worker" \
       --vpc-id "${VPC_ID}"
 
-  rm -f /tmp/workers-sg.json
-  cp workers-sg.json /tmp/workers-sg.json
-  sed -i "s|MASTER_SG|${K8S_MASTER_SG}|g" /tmp/workers-sg.json
-  sed -i "s|WORKER_SG|${K8S_WORKER_SG}|g" /tmp/workers-sg.json
-  sed -i "s|AWS_ACCT_ID|${AWS_ACCT_ID}|g" /tmp/workers-sg.json
-  sed -i "s|CLUSTER_NAME|${OCP_CLUSTER_NAME}|g" /tmp/workers-sg.json
-  sed -i "s|VPC_ID|${VPC_ID}|g" /tmp/workers-sg.json
-  sed -i "s|K8S_ELB_SG|${K8S_ELB_SG}|g" /tmp/workers-sg.json
-
-  aws --endpoint-url "${EC2_ENDPOINT}" ${AWS_OPTS} ec2 authorize-security-group-ingress \
-      --group-id ${K8S_WORKER_SG} \
-      --cli-input-json "$(cat /tmp/workers-sg.json)"
+  K8S_WORKER_SG=$(aws --endpoint-url "${EC2_ENDPOINT}" ${AWS_OPTS} ec2 describe-security-groups \
+    | jq '.SecurityGroups[] | select(.GroupName == "caas-k8s-worker") | .GroupId' | tr -d '"')
 
 fi
+
+rm -f /tmp/masters-sg.json
+cp masters-sg.json /tmp/masters-sg.json
+sed -i "s|MASTER_SG|${K8S_MASTER_SG}|g" /tmp/masters-sg.json
+sed -i "s|WORKER_SG|${K8S_WORKER_SG}|g" /tmp/masters-sg.json
+sed -i "s|AWS_ACCT_ID|${AWS_ACCT_ID}|g" /tmp/masters-sg.json
+sed -i "s|CLUSTER_NAME|${OCP_CLUSTER_NAME}|g" /tmp/masters-sg.json
+sed -i "s|VPC_ID|${VPC_ID}|g" /tmp/masters-sg.json
+sed -i "s|K8S_ELB_SG|${K8S_ELB_SG}|g" /tmp/masters-sg.json
+
+aws --endpoint-url "${EC2_ENDPOINT}" ${AWS_OPTS} ec2 authorize-security-group-ingress \
+    --group-id ${K8S_MASTER_SG} \
+    --cli-input-json "$(cat /tmp/masters-sg.json)"
+
+#rm -f /tmp/masters-sg.json
+
+rm -f /tmp/workers-sg.json
+cp workers-sg.json /tmp/workers-sg.json
+sed -i "s|MASTER_SG|${K8S_MASTER_SG}|g" /tmp/workers-sg.json
+sed -i "s|WORKER_SG|${K8S_WORKER_SG}|g" /tmp/workers-sg.json
+sed -i "s|AWS_ACCT_ID|${AWS_ACCT_ID}|g" /tmp/workers-sg.json
+sed -i "s|CLUSTER_NAME|${OCP_CLUSTER_NAME}|g" /tmp/workers-sg.json
+sed -i "s|VPC_ID|${VPC_ID}|g" /tmp/workers-sg.json
+sed -i "s|K8S_ELB_SG|${K8S_ELB_SG}|g" /tmp/workers-sg.json
+
+aws --endpoint-url "${EC2_ENDPOINT}" ${AWS_OPTS} ec2 authorize-security-group-ingress \
+    --group-id ${K8S_WORKER_SG} \
+    --cli-input-json "$(cat /tmp/workers-sg.json)"
 
 exit 0
