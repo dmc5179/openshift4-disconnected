7fd289aa3e8ecfb38a629a9c4b15f2b666db0592
Dan Clark <danclark@redhat.com>
2020-08-30 02:18:50 +0000

    Fix typo

--- scripts/operators/build_catalogs.sh
+++ scripts/operators/build_catalogs.sh
@@ -8,7 +8,7 @@ then
 
   echo "Building redhat-operators catalog image"
   /usr/local/bin/oc adm catalog build --insecure \
-      --registry-configa=${LOCAL_SECRET_JSON} \
+      --registry-config=${LOCAL_SECRET_JSON} \
       --appregistry-org redhat-operators "--to=${RH_OP_REPO}" \
       "--from=${OPERATOR_REGISTRY}" "--registry-config=${LOCAL_SECRET_JSON}"
 
@@ -19,7 +19,7 @@ then
 
   echo "Building certified operators catalog image"
   /usr/local/bin/oc adm catalog build --insecure \
-      --registry-configa=${LOCAL_SECRET_JSON} \
+      --registry-config=${LOCAL_SECRET_JSON} \
       --appregistry-org certified-operators "--to=${CERT_OP_REPO}" \
       "--from=${OPERATOR_REGISTRY}" "--registry-config=${LOCAL_SECRET_JSON}"
 
@@ -30,7 +30,7 @@ then
 
   echo "Building community operators catalog image"
   /usr/local/bin/oc adm catalog build --insecure \
-      --registry-configa=${LOCAL_SECRET_JSON} \
+      --registry-config=${LOCAL_SECRET_JSON} \
       --appregistry-org community-operators "--to=${COMM_OP_REPO}" \
       "--from=${OPERATOR_REGISTRY}" "--registry-config=${LOCAL_SECRET_JSON}"
 
