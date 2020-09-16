34026534b3e874dcd8ff7d390c868819b7055f52
Dan Clark <danclark@redhat.com>
2020-09-15 20:02:14 -0400

    Add script for configuring the default storage class

--- playbooks/roles/local_storage/files/set_default_sc.sh
+++ playbooks/roles/local_storage/files/set_default_sc.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-SC="localblock-sc"
+if [ $# -eq 0 ]
+  then
+    echo "No arguments supplied: Pass in the name of the storage class and true/false to set the default value"
+    echo "Usage: $0 <storage class name> <true/false>"
+fi
 
-oc patch storageclass "${SC}" -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
+SC="$1"
+DEFAULT="$2"
+
+oc patch storageclass "${SC}" -p "{\"metadata\": {\"annotations\":{\"storageclass.kubernetes.io/is-default-class\":\"${DEFAULT}\"}}}"
