6f8621babe34239e20002541407106cfa65bfe58
Dan Clark <danclark@redhat.com>
2020-10-10 00:41:46 +0000

    Use OC command from env file instead of relying on path or absolute path

--- scripts/operators/build_catalogs.sh
+++ scripts/operators/build_catalogs.sh
@@ -9,7 +9,7 @@ if [ "${RH_OP}" = true ]
 then
 
   echo "Building redhat-operators catalog image"
-  /usr/local/bin/oc adm catalog build --insecure \
+  ${OC} adm catalog build --insecure \
       --registry-config=${LOCAL_SECRET_JSON} \
       --appregistry-org redhat-operators "--to=${RH_OP_REPO}" \
       "--from=${OPERATOR_REGISTRY}" "--registry-config=${LOCAL_SECRET_JSON}"
@@ -20,7 +20,7 @@ if [ "${CERT_OP}" = true ]
 then
 
   echo "Building certified operators catalog image"
-  /usr/local/bin/oc adm catalog build --insecure \
+  ${OC} adm catalog build --insecure \
       --registry-config=${LOCAL_SECRET_JSON} \
       --appregistry-org certified-operators "--to=${CERT_OP_REPO}" \
       "--from=${OPERATOR_REGISTRY}" "--registry-config=${LOCAL_SECRET_JSON}"
@@ -31,7 +31,7 @@ if [ "${COMM_OP}" = true ]
 then
 
   echo "Building community operators catalog image"
-  /usr/local/bin/oc adm catalog build --insecure \
+  "${OC}" adm catalog build --insecure \
       --registry-config=${LOCAL_SECRET_JSON} \
       --appregistry-org community-operators "--to=${COMM_OP_REPO}" \
       "--from=${OPERATOR_REGISTRY}" "--registry-config=${LOCAL_SECRET_JSON}"
