2c78128cfc610d6a847513e25c543594935c98a7
Dan Clark <danclark@redhat.com>
2020-10-14 00:32:37 +0000

    Update to use ARN

--- scripts/installer/aws/govcloud/create_iam_roles.sh
+++ scripts/installer/aws/govcloud/create_iam_roles.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
+ACCOUNT_ID=$(aws sts get-caller-identity | jq -r '.Account')
+
 for policy in *_policy.json
 do
 
   POLICY_NAME=$(basename $policy _policy.json)
 
-  POLICY_ARN=$(aws iam create-policy --policy-name "${POLICY_NAME}" --policy-document "file://${PWD}/${policy}" | jq -r '.Policy.Arn')
+  aws iam create-policy --policy-name "${POLICY_NAME}" --policy-document "file://${PWD}/${policy}"
 
   aws iam create-user --user-name "${POLICY_NAME}"
 
-  aws iam attach-user-policy --policy-arn "${POLICY_ARN}" --user-name "${POLICY_NAME}"
+  aws iam attach-user-policy --policy-arn "arn:aws:iam::${ACCOUNT_ID}:policy/${POLICY_NAME}" --user-name "${POLICY_NAME}"
 
   aws iam create-access-key --user-name "${POLICY_NAME}" > "${POLICY_NAME}_keys.json"
 
