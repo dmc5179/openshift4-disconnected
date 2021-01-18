6acb9464e4b18858d7f9853a986f3fce1845d042
Dan Clark <danclark@redhat.com>
2021-01-17 22:56:08 -0500

    Placeholder

---
+++ scripts/mirroring/pull-redhat-operators.sh
@@ -0,0 +1,19 @@
+
+
+
+for o in $(find "${OCP_MEDIA_PATH}/redhat_operators_opm" -maxdepth 1 -type d | tail -n +2)
+do
+
+  CHANNEL=$(yq -r '.defaultChannel' "${o}/package.yaml")
+  VER=$(yq -r ".channels[] | select(.name==\"${CHANNEL}\") | .currentCSV" "${o}/package.yaml")
+
+  for f in $(find "${o}" -name "*clusterserviceversion.yaml")
+  do
+    if [[ $(yq -r ".metadata.name" "${f}") == "${VER}" ]]
+    then
+      yq -r '.spec.relatedImages[] | .image' "${f}"
+    fi
+  done
+
+done
+
