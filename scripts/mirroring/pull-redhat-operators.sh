39d000157b09094830aebb71319eaddc9bf16417
Dan Clark <danclark@redhat.com>
2021-01-19 10:26:56 -0500

    New way to pull only stable version of the operator images

--- scripts/mirroring/pull-redhat-operators.sh
+++ scripts/mirroring/pull-redhat-operators.sh
@@ -1,5 +1,10 @@
+#!/bin/bash -xe
 
+mkdir -p "${OCP_MEDIA_PATH}/redhat_operators_opm"
 
+opm index export \
+    --index=registry.redhat.io/redhat/redhat-operator-index:v4.6 \
+    --download-folder "${OCP_MEDIA_PATH}/redhat_operators_opm"
 
 for o in $(find "${OCP_MEDIA_PATH}/redhat_operators_opm" -maxdepth 1 -type d | tail -n +2)
 do
@@ -14,6 +19,5 @@ do
       yq -r '.spec.relatedImages[] | .image' "${f}"
     fi
   done
-
 done
 
