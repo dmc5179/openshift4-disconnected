19da6b4214a9b4b47ab9def0f3cfe1b0faf3a7bf
Dan Clark <danclark@redhat.com>
2021-01-19 23:01:38 -0500

    Add requirement to set account ID

--- scripts/installer/aws/manual_iam/create_iam_roles.sh
+++ scripts/installer/aws/manual_iam/create_iam_roles.sh
@@ -5,7 +5,15 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 # Source the environment file with the default settings
 source "${SCRIPT_DIR}/../../../env.sh"
 
+# Need to set or get the Account ID
 #ACCOUNT_ID=$(aws sts get-caller-identity | jq -r '.Account')
+
+
+if test -z "${ACCOUNT_ID}"
+then
+  echo "Account ID must be set"
+  exit 1
+fi
 
 for policy in *_policy.json
 do
@@ -21,3 +29,5 @@ do
   aws ${IAM_ENDPOINT} ${AWS_OPTS} iam create-access-key --user-name "${POLICY_NAME}" > "${POLICY_NAME}_keys.json"
 
 done
+
+exit 0
