9433551aa00d4dfbf80d01d4bf919c21c3c24550
Dan Clark <danclark@redhat.com>
2021-01-25 00:16:22 -0500

    Update ipa create script to use dynamic env source

--- scripts/ipa/ipa_dns.sh
+++ scripts/ipa/ipa_dns.sh
@@ -1,53 +1,79 @@
 #!/bin/bash
 
-OCP_DOMAIN='redhatgovsa.io'
-OCP_CLUSTER_NAME='caas'
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+# Source the environment file with the default settings
+source "${SCRIPT_DIR}/../env.sh"
 
 # Command to install an IPA Server
-ipa-server-install -p 'COMPLEX_PASSWORD' -a 'COMPLEX_PASSWORD' --setup-dns --hostname=ipa-master.${OCP_CLUSTER_NAME}.${OCP_DOMAIN} --ip-address=10.0.106.35 --no-forwarder --auto-reverse --no-dnssec-validation --domain=${OCP_DOMAIN} --realm=${OCP_DOMAIN_UPPER}
+#ipa-server-install -p 'COMPLEX_PASSWORD' -a 'COMPLEX_PASSWORD' --setup-dns \
+#    --hostname=ipa-master.${OCP_CLUSTER_NAME}.${OCP_DOMAIN} --ip-address=10.0.106.35 \
+#    --no-forwarder --auto-reverse --no-dnssec-validation --domain=${OCP_DOMAIN} \
+#    --realm=${OCP_DOMAIN_UPPER}
+#ipa dnsrecord-add "${BASE_DOMAIN}" ipa-master.${OCP_CLUSTER_NAME} --a-rec
 
+set -x
 
-ipa dnsrecord-add "${BASE_DOMAIN}" ipa-master.${OCP_CLUSTER_NAME} --a-rec 10.0.106.35
+CLUSTER_ZONE="${OCP_CLUSTER_NAME}.${OCP_BASE_DOMAIN}"
+
+ipa dnszone-add "${CLUSTER_ZONE}"
 
 # pointers to load api load balancer
-ipa dnsrecord-add "${BASE_DOMAIN}" api.${OCP_CLUSTER_NAME} --a-rec 10.0.106.41
-ipa dnsrecord-add "${BASE_DOMAIN}" api-int.${OCP_CLUSTER_NAME} --a-rec 10.0.106.41
+ipa dnsrecord-add "${CLUSTER_ZONE}" api --a-rec "${API_LB_IP}"
+ipa dnsrecord-add "${CLUSTER_ZONE}" api-int --a-rec "${API_INT_LB_IP}"
 
 # The wildcard also points to the load balancer
-ipa dnsrecord-add "${BASE_DOMAIN}" *.apps.${OCP_CLUSTER_NAME} --a-rec 10.0.106.42
+ipa dnsrecord-add "${CLUSTER_ZONE}" *.apps --a-rec "${APPS_LB_IP}"
 
 # Create entry for the bootstrap host
-ipa dnsrecord-add "${BASE_DOMAIN}" bootstrap.${OCP_CLUSTER_NAME} --a-rec 10.0.106.50
+ipa dnsrecord-add "${CLUSTER_ZONE}" bootstrap --a-rec "${BOOTSTRAP_IP}"
 
 # Create entries for the master hosts
-ipa dnsrecord-add "${BASE_DOMAIN}" master0.${OCP_CLUSTER_NAME} --a-rec 10.0.106.51
-ipa dnsrecord-add "${BASE_DOMAIN}" master1.${OCP_CLUSTER_NAME} --a-rec 10.0.106.52
-ipa dnsrecord-add "${BASE_DOMAIN}" master2.${OCP_CLUSTER_NAME} --a-rec 10.0.106.53
+ipa dnsrecord-add "${CLUSTER_ZONE}" master0 --a-rec "${MASTER0_IP}"
+ipa dnsrecord-add "${CLUSTER_ZONE}" master1 --a-rec "${MASTER1_IP}"
+ipa dnsrecord-add "${CLUSTER_ZONE}" master2 --a-rec "${MASTER2_IP}"
 
 # Create entries for the worker hosts
-ipa dnsrecord-add "${BASE_DOMAIN}" worker0.${OCP_CLUSTER_NAME} --a-rec 10.0.106.61
-ipa dnsrecord-add "${BASE_DOMAIN}" worker1.${OCP_CLUSTER_NAME} --a-rec 10.0.106.62
-ipa dnsrecord-add "${BASE_DOMAIN}" worker2.${OCP_CLUSTER_NAME} --a-rec 10.0.106.63
+ipa dnsrecord-add "${CLUSTER_ZONE}" worker0 --a-rec "${WORKER0_IP}"
+ipa dnsrecord-add "${CLUSTER_ZONE}" worker1 --a-rec "${WORKER1_IP}"
+ipa dnsrecord-add "${CLUSTER_ZONE}" worker2 --a-rec "${WORKER2_IP}"
 
 # The ETCd cluster lives on the masters...so point these to the IP of the masters
-ipa dnsrecord-add "${BASE_DOMAIN}" etcd-0.${OCP_CLUSTER_NAME} --a-rec 10.0.106.51
-ipa dnsrecord-add "${BASE_DOMAIN}" etcd-1.${OCP_CLUSTER_NAME} --a-rec 10.0.106.52
-ipa dnsrecord-add "${BASE_DOMAIN}" etcd-2.${OCP_CLUSTER_NAME} --a-rec 10.0.106.53
+ipa dnsrecord-add "${CLUSTER_ZONE}" etcd-0 --a-rec "${MASTER0_IP}"
+ipa dnsrecord-add "${CLUSTER_ZONE}" etcd-1 --a-rec "${MASTER1_IP}"
+ipa dnsrecord-add "${CLUSTER_ZONE}" etcd-2 --a-rec "${MASTER2_IP}"
 
 # The SRV records ...note the trailing dot at the end.
-ipa dnsrecord-add ${BASE_DOMAIN} --srv-rec=_etcd-server-ssl._tcp.${OCP_CLUSTER_NAME} --srv-priority=0 --srv-weight=10 --srv-port=2380 --srv-target=etcd-0.${OCP_CLUSTER_NAME}.redhatgovsa.io.
-ipa dnsrecord-add ${BASE_DOMAIN} --srv-rec=_etcd-server-ssl._tcp.${OCP_CLUSTER_NAME} --srv-priority=0 --srv-weight=10 --srv-port=2380 --srv-target=etcd-1.${OCP_CLUSTER_NAME}.redhatgovsa.io.
-ipa dnsrecord-add ${BASE_DOMAIN} --srv-rec=_etcd-server-ssl._tcp.${OCP_CLUSTER_NAME} --srv-priority=0 --srv-weight=10 --srv-port=2380 --srv-target=etcd-2.${OCP_CLUSTER_NAME}.redhatgovsa.io.
+# Not required since OpenShift 4.3
+#ipa dnsrecord-add ${CLUSTER_ZONE} --srv-rec=_etcd-server-ssl._tcp.${OCP_CLUSTER_NAME} --srv-priority=0 --srv-weight=10 --srv-port=2380 --srv-target=etcd-0.${OCP_CLUSTER_NAME}.${OCP_BASE_DOMAIN}.
+#ipa dnsrecord-add ${CLUSTER_ZONE} --srv-rec=_etcd-server-ssl._tcp.${OCP_CLUSTER_NAME} --srv-priority=0 --srv-weight=10 --srv-port=2380 --srv-target=etcd-1.${OCP_CLUSTER_NAME}.${OCP_BASE_DOMAIN}.
+#ipa dnsrecord-add ${CLUSTER_ZONE} --srv-rec=_etcd-server-ssl._tcp.${OCP_CLUSTER_NAME} --srv-priority=0 --srv-weight=10 --srv-port=2380 --srv-target=etcd-2.${OCP_CLUSTER_NAME}.${OCP_BASE_DOMAIN}.
+#
+#ipa dnsrecord-add "${CLUSTER_ZONE}" _etcd-server-ssl._tcp.${OCP_CLUSTER_NAME} --srv-rec="0 10 2380 etcd-0.${OCP_CLUSTER_NAME}.${OCP_BASE_DOMAIN}."
+#ipa dnsrecord-add "${CLUSTER_ZONE}" _etcd-server-ssl._tcp.${OCP_CLUSTER_NAME} --srv-rec="0 10 2380 etcd-1.${OCP_CLUSTER_NAME}.${OCP_BASE_DOMAIN}."
+#ipa dnsrecord-add "${CLUSTER_ZONE}" _etcd-server-ssl._tcp.${OCP_CLUSTER_NAME} --srv-rec="0 10 2380 etcd-2.${OCP_CLUSTER_NAME}.${OCP_BASE_DOMAIN}."
 
-ipa dnsrecord-add "${BASE_DOMAIN}" _etcd-server-ssl._tcp.${OCP_CLUSTER_NAME} --srv-rec="0 10 2380 etcd-0.${OCP_CLUSTER_NAME}.redhatgovsa.io."
-ipa dnsrecord-add "${BASE_DOMAIN}" _etcd-server-ssl._tcp.${OCP_CLUSTER_NAME} --srv-rec="0 10 2380 etcd-1.${OCP_CLUSTER_NAME}.redhatgovsa.io."
-ipa dnsrecord-add "${BASE_DOMAIN}" _etcd-server-ssl._tcp.${OCP_CLUSTER_NAME} --srv-rec="0 10 2380 etcd-2.${OCP_CLUSTER_NAME}.redhatgovsa.io."
+ipa dnsrecord-add $(echo "${MASTER0_IP}" | awk -F. -vOFS=. '{print $3,$2,$1,"in-addr.arpa."}') \
+                  $(echo $MASTER0_IP | awk -F\. '{print $NF}') --ptr-rec=master0.${CLUSTER_ZONE}.
 
-#ipa dnsrecord-del 106.0.10.in-addr.arpa. --ptr-rec=master0 ${OCP_CLUSTER_NAME}.redhatgovsa.io.
-ipa dnsrecord-add 106.0.10.in-addr.arpa. 51 --ptr-rec=master0.${OCP_CLUSTER_NAME}.redhatgovsa.io.
-ipa dnsrecord-add 106.0.10.in-addr.arpa. 52 --ptr-rec=master1.${OCP_CLUSTER_NAME}.redhatgovsa.io.
-ipa dnsrecord-add 106.0.10.in-addr.arpa. 53 --ptr-rec=master2.${OCP_CLUSTER_NAME}.redhatgovsa.io.
-ipa dnsrecord-add 106.0.10.in-addr.arpa. 61 --ptr-rec=worker0.${OCP_CLUSTER_NAME}.redhatgovsa.io.
-ipa dnsrecord-add 106.0.10.in-addr.arpa. 62 --ptr-rec=worker1.${OCP_CLUSTER_NAME}.redhatgovsa.io.
-ipa dnsrecord-add 106.0.10.in-addr.arpa. 63 --ptr-rec=worker2.${OCP_CLUSTER_NAME}.redhatgovsa.io.
-ipa dnsrecord-add 106.0.10.in-addr.arpa. 50 --ptr-rec=bootstrap.${OCP_CLUSTER_NAME}.redhatgovsa.io.
+ipa dnsrecord-add $(echo "${MASTER1_IP}" | awk -F. -vOFS=. '{print $3,$2,$1,"in-addr.arpa."}') \
+                  $(echo $MASTER1_IP | awk -F\. '{print $NF}') --ptr-rec=master1.${CLUSTER_ZONE}.
+
+ipa dnsrecord-add $(echo "${MASTER2_IP}" | awk -F. -vOFS=. '{print $3,$2,$1,"in-addr.arpa."}') \
+                  $(echo $MASTER2_IP | awk -F\. '{print $NF}') --ptr-rec=master2.${CLUSTER_ZONE}.
+
+ipa dnsrecord-add $(echo "${WORKER0_IP}" | awk -F. -vOFS=. '{print $3,$2,$1,"in-addr.arpa."}') \
+                  $(echo $WORKER0_IP | awk -F\. '{print $NF}') --ptr-rec=worker0.${CLUSTER_ZONE}.
+
+ipa dnsrecord-add $(echo "${WORKER1_IP}" | awk -F. -vOFS=. '{print $3,$2,$1,"in-addr.arpa."}') \
+                  $(echo $WORKER1_IP | awk -F\. '{print $NF}') --ptr-rec=worker1.${CLUSTER_ZONE}.
+
+ipa dnsrecord-add $(echo "${WORKER2_IP}" | awk -F. -vOFS=. '{print $3,$2,$1,"in-addr.arpa."}') \
+                  $(echo $WORKER2_IP | awk -F\. '{print $NF}') --ptr-rec=worker2.${CLUSTER_ZONE}.
+
+ipa dnsrecord-add $(echo "${BOOTSTRAP_IP}" | awk -F. -vOFS=. '{print $3,$2,$1,"in-addr.arpa."}') \
+                  $(echo $BOOTSTRAP_IP | awk -F\. '{print $NF}') --ptr-rec=bootstrap.${CLUSTER_ZONE}.
+
+exit 0
+# Example ipa commands
+#ipa dnsrecord-del 106.0.10.in-addr.arpa. --ptr-rec=master0 ${OCP_CLUSTER_NAME}.${OCP_BASE_DOMAIN}.
