21161f2242307f91dea8eb8708bd237cdec8064c
Dan Clark <danclark@redhat.com>
2021-02-14 00:56:58 -0500

    Add scripts for connectiong OCP to IPA

--- scripts/oauth/ldap_sync.sh
+++ scripts/oauth/ldap_sync.sh
@@ -33,3 +33,31 @@ EOF
 
 
 oc adm groups sync --sync-config=ldapgroupsync.yaml --confirm
+
+
+
+
+exit 0
+
+
+
+#########
+git clone https://github.com/redhat-cop/openshift-management.git
+oc login -u system:admin
+oc adm new-project openshift-cluster-ops
+oc process --local \
+  -f openshift-management/jobs/cronjob-ldap-group-sync-secure.yml \
+  -p NAMESPACE='openshift-cluster-ops' \
+  -p LDAP_URL=ldap://ipa.shared.example.opentlc.com \
+  -p LDAP_BIND_DN=uid=admin,cn=users,cn=accounts,dc=shared,dc=example,dc=opentlc,dc=com \
+  -p LDAP_BIND_PASSWORD='r3dh4t1!' \
+  -p LDAP_CA_CERT="$(cat ipa-ca.crt)" \
+  -p LDAP_GROUP_UID_ATTRIBUTE='dn' \
+  -p LDAP_GROUPS_FILTER='(objectClass=groupofnames)' \
+  -p LDAP_GROUPS_SEARCH_BASE='cn=groups,cn=accounts,dc=shared,dc=example,dc=opentlc,dc=com' \
+  -p LDAP_GROUPS_WHITELIST="$(cat whitelist.txt)" \
+  -p LDAP_USERS_SEARCH_BASE='cn=users,cn=accounts,dc=shared,dc=example,dc=opentlc,dc=com' \
+  -p SCHEDULE='*/5 * * * *' \
+| oc apply -f -
+oc get pod -n openshift-cluster-ops
+oc logs cronjob-ldap-group-sync-<epoch>-<guid> -n openshift-cluster-ops  # from output above
