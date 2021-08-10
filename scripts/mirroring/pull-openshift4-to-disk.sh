e78d792fe0f1d6f85b0164e273ce0a668f34602b
Dan Clark <danclark@redhat.com>
2021-08-10 10:49:10 -0400

    Simplify script for mirroring OpenShift 4 releases to a local directory

--- scripts/mirroring/pull-openshift4-to-disk.sh
+++ scripts/mirroring/pull-openshift4-to-disk.sh
@@ -1,19 +1,31 @@
 #!/bin/bash -e
 
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+export OCP_RELEASE="4.6.9"
+export OCP_ARCH="x86_64"
 
-# Source the environment file with the default settings
-source "${SCRIPT_DIR}/../env.sh"
+# Directory where OCP images are written to or read from
+# When mirroring to disk
+export OCP_MEDIA_PATH="/opt/ocp-${OCP_RELEASE}"
+
+# This needs to be a pull secret that combines the pull secret from Red Hat
+# to pull all the images down and a pull secret from your local registry so we
+# can push to it
+export LOCAL_SECRET_JSON="${HOME}/pull-secret.json"
+
+######
+# Unlikely that these need to be changed
+export RELEASE_NAME="ocp-release"
+export UPSTREAM_REPO='openshift-release-dev'
+######
 
 set -x
 
 #############################################################
 
 mkdir -p "${OCP_MEDIA_PATH}/mirror"
-mkdir -p "${RHCOS_MEDIA_PATH}"
 
 echo "Mirroring cluster images"
-${OC} adm release mirror -a ${LOCAL_SECRET_JSON} \
+oc adm release mirror -a ${LOCAL_SECRET_JSON} \
   --from=quay.io/${UPSTREAM_REPO}/${RELEASE_NAME}:${OCP_RELEASE}-${OCP_ARCH} \
   --to file://openshift/release \
   --to-dir=${OCP_MEDIA_PATH}/mirror
