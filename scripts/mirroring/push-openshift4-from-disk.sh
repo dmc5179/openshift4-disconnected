5b47711a7def059296cb3818e27e46d389e98f46
Dan Clark <danclark@redhat.com>
2021-08-10 10:52:30 -0400

    Simplify push from disk script

--- scripts/mirroring/push-openshift4-from-disk.sh
+++ scripts/mirroring/push-openshift4-from-disk.sh
@@ -1,13 +1,25 @@
-#!/bin/bash -xe
+#!/bin/bash -e
 
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+export OCP_RELEASE="4.6.9"
 
-# Source the environment file with the default settings
-source "${SCRIPT_DIR}/../env.sh"
+# Directory where OCP images are written to or read from
+# When mirroring to disk
+export OCP_MEDIA_PATH="/opt/ocp-${OCP_RELEASE}"
+
+# This needs to be a pull secret that combines the pull secret from Red Hat
+# to pull all the images down and a pull secret from your local registry so we
+# can push to it
+export LOCAL_SECRET_JSON="${HOME}/pull-secret.json"
+
+# Registry to push the OpenShift 4 container images to 
+export LOCAL_REG='localhost:5000'
+
+# Repository in the registry where the OpenShift 4 images will be pushed
+export LOCAL_REPO='ocp4/openshift4'
 
 set -x
 
-${OC} image mirror \
+oc image mirror \
   --registry-config=${LOCAL_SECRET_JSON} \
   --from-dir=${OCP_MEDIA_PATH}/mirror \
   "file://openshift/release:${OCP_RELEASE}*" \
