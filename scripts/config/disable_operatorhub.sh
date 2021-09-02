cc871e37342e12aebc3331dda8ed585cde673d2e
Dan Clark <danclark@redhat.com>
2021-09-02 14:41:06 +0000

    Comment out unused lines

--- scripts/config/disable_operatorhub.sh
+++ scripts/config/disable_operatorhub.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+#SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # Source the environment file with the default settings
-source "${SCRIPT_DIR}/../env.sh"
+#source "${SCRIPT_DIR}/../env.sh"
 
 # Disable the OpenShift integrated OperatorHub.
 # Typically done for disconnected/air-gap installs until the disconnected
