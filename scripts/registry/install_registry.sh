f1dc5ba3cf960c6b80331e6f5e077d3187425900
Dan Clark <danclark@redhat.com>
2021-10-27 08:01:52 -0400

    Simplify registry installer script

--- scripts/registry/install_registry.sh
+++ scripts/registry/install_registry.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -e
 
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+#SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
   
 # Source the environment file with the default settings
-source "${SCRIPT_DIR}/../env.sh"
+#source "${SCRIPT_DIR}/../env.sh"
 
 export REGISTRY_DIR="${HOME}/registry/"
 export REGISTRY_HOSTNAME="${HOSTNAME}"
@@ -15,7 +15,12 @@ export REGISTRY_IMG="docker.io/library/registry:2"
 export REG_USER=$(id -un)
 export REG_GROUP=$(id -gn)
 
-sudo yum -y install podman httpd httpd-tools firewalld skopeo
+# Registry username and password
+REG_USERNAME='dummy'
+REG_PASSWORD='dummy'
+
+# Install required dependencies
+sudo yum -y install podman httpd httpd-tools firewalld
 
 mkdir -p ${REGISTRY_DIR}/{auth,certs,data}
 
@@ -35,13 +40,17 @@ fi
 # Print the certificate
 openssl x509 -in "${REGISTRY_DIR}/certs/registry.crt" -text -noout
 
-htpasswd -bBc ${REGISTRY_DIR}/auth/htpasswd dummy dummy
+# Generate the registry htpasswd file
+htpasswd -bBc ${REGISTRY_DIR}/auth/htpasswd "${REG_USERNAME}" "${REG_PASSWORD}"
 
 #Make sure to trust the self signed cert we just made
 sudo cp -f ${REGISTRY_DIR}/certs/registry.crt /etc/pki/ca-trust/source/anchors/
 sudo update-ca-trust
 sudo update-ca-trust extract
 
+# Configure the firewall
+sudo systemctl enable firewalld
+sudo systemctl restart firewalld
 sudo firewall-cmd --add-port=${REGISTRY_PORT}/tcp --zone=internal --permanent
 sudo firewall-cmd --add-port=${REGISTRY_PORT}/tcp --zone=public   --permanent
 sudo firewall-cmd --add-service=http  --permanent
@@ -69,29 +78,26 @@ podman run --name registry_server -p ${REGISTRY_PORT}:5000 \
 # Configure SELinux to allow containers in systemd services
 sudo setsebool -P container_manage_cgroup on
 
-sudo bash -c "cat <<EOF >> /etc/systemd/system/registry-container.service
-
-[Unit]
-Description=Container Registry
-
-[Service]
-Restart=always
-ExecStart=/usr/bin/podman start -a registry_server
-ExecStop=/usr/bin/podman stop -t 15 registry_server
-User=${REG_USER}
-Group=${REG_GROUP}
-
-[Install]
-WantedBy=local.target
-
-EOF"
-
-sudo systemctl daemon-reload
-sudo systemctl enable registry-container.service
+# Configure the systemd service in either user or system space
+# depending on who is running this script
+if [ $(id -u) -gt 0 ]
+then
+  mkdir -p ~/.config/systemd/user
+  podman generate systemd --name registry_server > ~/.config/systemd/user/registry-container.service
+  systemctl --user daemon-reload
+  # Stop the server so systemd can take over
+  podman stop registry_server
+  systemctl --user enable --now registry-container.service
+else
+  podman generate systemd --name registry_server > /etc/systemd/system/registry-container.service
+  systemctl daemon-reload
+  # Stop the server so systemd can take over
+  systemctl enable --now registry-container.service
+fi
 
 # Test the connection
 echo "Testing connection by hostname:"
-curl -u dummy:dummy https://${REGISTRY_HOSTNAME}:${REGISTRY_PORT}/v2/_catalog
+curl -u "${REG_USERNAME}:${REG_PASSWORD}" https://${REGISTRY_HOSTNAME}:${REGISTRY_PORT}/v2/_catalog
 
 echo 'Testing connection by IP Address'
-curl -u dummy:dummy https://${REGISTRY_IP}:${REGISTRY_PORT}/v2/_catalog
+curl -u "${REG_USERNAME}:${REG_PASSWORD}" https://${REGISTRY_IP}:${REGISTRY_PORT}/v2/_catalog
