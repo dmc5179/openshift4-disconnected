afcc7fa659778f8e16d1f5ce5728d59208c6154b
Dan Clark <danclark@redhat.com>
2025-09-26 06:15:52 -0400

    Make image removal generic

--- oc-mirror/clean-image-from-nodes.sh
+++ oc-mirror/clean-image-from-nodes.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 
+IMAGE="redhat-operator-index"
+
 for node in $(oc get nodes --no-headers=true -o custom-columns=NAME:.metadata.name)
 do
   echo "Cleaning node: $node"
 
-  oc debug -q node/$node -- chroot /host bash -c "podman images | grep redhat-operator-index | tail -1 | awk '{print $3}' | xargs -r podman rmi"
+  oc debug -q node/$node -- chroot /host bash -c "podman images | grep ${IMAGE}  | tail -1 | awk '{print $3}' | xargs -r podman rmi"
 
 done
+
+exit 0
+
+
+# Old way of doing this without the substitution above which I haven't tested
+#for node in $(oc get nodes --no-headers=true -o custom-columns=NAME:.metadata.name)
+#do
+#  echo "Cleaning node: $node"
+#
+#  oc debug -q node/$node -- chroot /host bash -c "podman images | grep redhat-operator-index | tail -1 | awk '{print $3}' | xargs -r podman rmi"
+#
+#done
