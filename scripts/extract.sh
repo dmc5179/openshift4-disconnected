84138e7e2b2973074acc14e570e946163764b9dc
Dan Clark <danclark@redhat.com>
2025-09-30 12:22:33 -0400

    Updated oc adm extract script

--- scripts/extract.sh
+++ scripts/extract.sh
@@ -1,15 +1,24 @@
-#!/bin/bash
+#!/bin/bash -xe
 
-PULL_SECRET=/home/user/pull-secret
-OCP_VERSION=4.19.7
+# Pull secret with auth to the offline registry
+PULL_SECRET=/home/ec2-user/quay-mirror-auth.json
+OCP_VERSION=4.19.13
 ARCH=x86_64
-PRIVATE_REGISTRY="myregistry.com:8443"
+PRIVATE_REGISTRY="ip-10-0-104-9.us-east-2.compute.internal:8443"
+OC_MIRROR_WORKING_DIR="/home/ec2-user/download1/working-dir"
 
-# Command to extract from public quay registry
-#oc adm release extract --registry-config=${PULL_SECRET} --tools quay.io/openshift-release-dev/ocp-release:${OCP_VERSION}-${ARCH}
+idms_location="cluster-resources/idms-oc-mirror.yaml"
 
+IDMS_TMP=$(mktemp)
 
-# oc-mirror by default will put the image above into openshift/release-images
-# so the command looks like
+# Use this to get the IDMS for extract out of the oc-mirror metadata files on the registry server
+yq '. | select(.metadata.name == "idms-release-0")' "${OC_MIRROR_WORKING_DIR}/${idms_location}" > ${IDMS_TMP}
 
-oc adm release extract --registry-config=${PULL_SECRET} --tools "${PRIVATE_REGISTRY}/openshift/release-images:${OCP_VERSION}-${ARCH}"
+# Extracts all the tools but this does not include openshift-install-fips which it should. Bug?
+oc adm release extract -a ${PULL_SECRET} --idms-file=${IDMS_TMP} --tools "${PRIVATE_REGISTRY}/openshift/release-images:${OCP_VERSION}-${ARCH}"
+
+oc adm release extract -a ${PULL_SECRET} --idms-file=${IDMS_TMP} --command=openshift-install-fips "${PRIVATE_REGISTRY}/openshift/release-images:${OCP_VERSION}-${ARCH}"
+
+rm -f ${IDMS_TMP}
+exit 0
+
