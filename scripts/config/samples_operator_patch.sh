c2ce924cb888334d728848fa13444de977bff463
Dan Clark <danclark@redhat.com>
2025-12-23 11:32:54 -0500

    Update command to disable default samples operator for disconnected clusters

--- scripts/config/samples_operator_patch.sh
+++ scripts/config/samples_operator_patch.sh
@@ -1,9 +1,8 @@
 #!/bin/bash -xe
 
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+#SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # Source the environment file with the default settings
-source "${SCRIPT_DIR}/../env.sh"
+#source "${SCRIPT_DIR}/../env.sh"
 
-${OC} patch configs.samples.operator.openshift.io cluster --type merge \
-  --patch "{\"spec\":{\"samplesRegistry\": \"${REMOTE_REG}\", \"managementState\": \"Managed\"}}"
+oc patch configs.samples.operator.openshift.io cluster --type='merge' -p='{"spec":{"managementState":"Removed"}}'
